0ed561a346650bc97a5d9bab70197e78406ea1c7
shups <shuloh>
2019-10-11 00:03:12 +0800

    added .env config file that uses development mnemonic phrase for ganache-cli

---
+++ README.md
@@ -0,0 +1,7 @@
+# Development
+
+In the project root directory, create a new .env file and set the MNEMONIC variable with your 12 word seed phrase.
+
+`npm run ganache`
+
+The above commands runs ganache-cli
---
+++ runGanacheCli.js
@@ -0,0 +1,25 @@
+const PROCESSNAME = "ganache-cli";
+require("dotenv").config();
+const execSync = require("child_process").execSync;
+const path = require("path");
+
+/**
+ * Creates a path to an executable in the node_modules/.bin directory. Each
+ * path segment is joined with the appropriate platform-specific separator as
+ * a delimiter.
+ * @param {String} cmd The name of the executable.
+ * @returns {String} The path to the executable.
+ */
+function getBinFile(cmd) {
+  return path.join("node_modules", ".bin", cmd);
+}
+
+// Execute the command...
+execSync(
+  `${getBinFile(PROCESSNAME)} -m \"${process.env.MNEMONIC}\" -i ${
+    process.env.NETWORKID
+  }`,
+  {
+    stdio: [0, 1, 2]
+  }
+);
